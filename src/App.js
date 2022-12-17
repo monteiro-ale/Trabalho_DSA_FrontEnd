c55866de7ae56082e261aff820f4a793bcd197e3
monteiro-ale <monteiro.tec32@gmail.com>
2022-12-17 17:42:07 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [values, setValues] = useState();
+  // console.log(values);
+
+  const mudancaValores = (value) =>{
+    setValues(prevValue =>({
+      ...prevValue,
+      [value.target.name]: value.target.value
+    }));
+  }
+  const handleClickButton = () =>{
+    console.log(values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+     <div className="register-container">
+      <h1 className="titulo-livraria">Livraria</h1>
+      <input type="text" name="isbnlivro" placeholder="International Standard Book Number" className="cadastrolivros" onChange={mudancaValores} />
+
+      <input type="text" name="titulolivro" placeholder="Título do Livro" className="cadastrolivros" onChange={mudancaValores} />
+
+      <input type="text" name="idautor" placeholder="Id Autor" className="cadastrolivros" onChange={mudancaValores} />
+
+      <input type="text" name="editoralivro" placeholder="Editora" className="cadastrolivros" onChange={mudancaValores} />
+
+      <input type="text" name="anolivro" placeholder="Data de Lançamento" className="cadastrolivros" onChange={mudancaValores} />
+
+      <input type="text" name="qtdelivrodisponivel" placeholder="Quantidade disponível" className="cadastrolivros" onChange={mudancaValores} />
+
+      <button className="button" onClick={() => handleClickButton()}>Cadastrar Livro</button>
+
+     </div>
     </div>
   );
 }
