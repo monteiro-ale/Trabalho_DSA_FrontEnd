56396c28faf9f90a27464daaa2c6ea19c1770b86
monteiro-ale <monteiro.tec32@gmail.com>
2022-12-19 13:25:57 -0300

    Criando requests de inserção

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Axios from "axios";
+import Card from "./components/cards/card";
 
 function App() {
 
   const [values, setValues] = useState();
-  // console.log(values);
+  //  console.log(values);
 
   const mudancaValores = (value) =>{
     setValues(prevValue =>({
@@ -13,13 +15,34 @@ function App() {
     }));
   }
   const handleClickButton = () =>{
-    console.log(values);
+    Axios.post("http://localhost:3001/api/books/",
+    {
+      isbnlivro: values.isbnlivro,
+      titulolivro: values.titulolivro,
+      idautor: values.idautor,
+      editoralivro: values.editoralivro,
+      anolivro: values.anolivro,
+      qtdelivrodisponivel: values.qtdelivrodisponivel
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+    ).then((response)=> {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
   };
+
 
   return (
     <div className="app-container">
      <div className="register-container">
       <h1 className="titulo-livraria">Livraria</h1>
+
       <input type="text" name="isbnlivro" placeholder="International Standard Book Number" className="cadastrolivros" onChange={mudancaValores} />
 
       <input type="text" name="titulolivro" placeholder="Título do Livro" className="cadastrolivros" onChange={mudancaValores} />
@@ -35,6 +58,7 @@ function App() {
       <button className="button" onClick={() => handleClickButton()}>Cadastrar Livro</button>
 
      </div>
+     <Card></Card>
     </div>
   );
 }
